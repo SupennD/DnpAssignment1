983f01724f323335bf435527bfc46294762a28c5
PawsitiveCoder <167691828+PawsitiveCoder@users.noreply.github.com>
2024-09-10 11:36:41 +0200

    add user repository

---
+++ Server/RepositoryContracts/IUserRepository.cs
@@ -0,0 +1,12 @@
+using Entities;
+
+namespace RepositoryContracts;
+
+public interface IUserRepository
+{
+    Task<User> AddAsync (User user);
+    Task UpdateAsync (User user);
+    Task DeleteAsync (int id);
+    Task<User> GetSingleAsync (int id);
+    IQueryable<User> GetMany();
+}
