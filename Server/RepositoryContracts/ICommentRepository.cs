bddd83fd2fbc02dcc2da7b81dd3472b81b783cb5
Eser Sarıyar <esersariyar@gmail.com>
2024-09-10 11:41:16 +0200

    Fix Comment

---
+++ Server/RepositoryContracts/ICommentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+
+namespace RepositoryContracts;
+
+public interface ICommentRepository
+{
+    Task<Post> AddComment(ICommentRepository commentRepository);
+    Task UpdateAsync(ICommentRepository commentRepository);
+    Task DeleteAsync(ICommentRepository commentRepository);
+    Task<Post> GetPostByIdAsync(Guid postId);
+    IQueryable<Post> GetPosts();
+}
