eaeb9086fc2d31298ed409081edb93d3551b4fa7
Eser Sarıyar <esersariyar@gmail.com>
2024-09-10 11:35:48 +0200

    Update Comment.cs interface

---
+++ Server/RepositoryContracts/Comment.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+
+namespace RepositoryContracts;
+
+public interface Comment
+{
+    Task<Post> AddComment(Post post);
+    Task UpdateAsync(Comment comment);
+    Task DeleteAsync(Comment comment);
+    Task<Post> GetPostByIdAsync(Guid postId);
+    IQueryable<Post> GetPosts();
+}
