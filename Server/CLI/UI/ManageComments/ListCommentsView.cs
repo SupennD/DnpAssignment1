657ea566b5ad696a1699b4851d5cecdb141a26b4
funduza <buruncukdora@gmail.com>
2024-09-16 20:31:52 +0200

    Add ListComments and ListUsers View.

---
+++ Server/CLI/UI/ManageComments/ListCommentsView.cs
@@ -0,0 +1,34 @@
+﻿using Entities;
+
+using RepositoryContracts;
+
+namespace CLI.UI.ManageComments;
+
+public class ListCommentsView
+{
+    private readonly ICommentRepository _commentRepository;
+
+    public ListCommentsView(ICommentRepository commentRepository)
+    {
+        _commentRepository = commentRepository;
+    }
+
+    public Task ShowListCommentViewAsync()
+    {
+        IQueryable<Comment> queryableComment = _commentRepository.GetMany();
+
+        if (!queryableComment.Any())
+        {
+            Console.WriteLine("There are no comments.");
+        }
+
+
+        foreach (var comment in queryableComment)
+        {
+            Console.WriteLine("Id: " + comment.Id + " Body: " + comment.Body);
+        }
+
+        return Task.CompletedTask;
+    }
+
+}
