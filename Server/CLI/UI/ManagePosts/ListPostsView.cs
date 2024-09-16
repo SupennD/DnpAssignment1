33d1fb22320f6b5633af839be0f1617219425a65
funduza <buruncukdora@gmail.com>
2024-09-16 11:11:40 +0200

    Create ListPostView

---
+++ Server/CLI/UI/ManagePosts/ListPostsView.cs
@@ -0,0 +1,33 @@
+﻿using Entities;
+
+using RepositoryContracts;
+
+namespace CLI.UI.ManagePosts;
+
+public class ListPostsView
+{
+    private readonly IPostRepository postRepository;
+
+    public ListPostsView(IPostRepository postRepository)
+    {
+        this.postRepository = postRepository;
+    }
+
+    public void ShowListPostView(IPostRepository postRepository)
+    {
+        IQueryable<Post> queryablePost = postRepository.GetMany();
+
+        if (!queryablePost.Any())
+        {
+            Console.WriteLine("There are no posts");
+        }
+
+
+        foreach (var post in queryablePost)
+        {
+            Console.WriteLine("Id: " +post.Id + " Title: " + post.Title);
+        }
+
+    }
+    
+}
