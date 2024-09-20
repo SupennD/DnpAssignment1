630f44fef4a5d14cb05fc0ce9f19237276d6dcdb
SupennD <151931581+SupennD@users.noreply.github.com>
2024-09-20 11:21:07 +0200

    add UserFileRepository.cs

---
+++ Server/FileRepositories/UserFileRepository.cs
@@ -0,0 +1,64 @@
+using Entities;
+
+using RepositoryContracts;
+
+namespace FileRepositories;
+
+public class UserFileRepository : GenericFileRepository<User>, IUserRepository
+{
+    protected override string FilePath { get; } = "users.json";
+
+    public override async Task<User> AddAsync(User entity)
+    {
+        List<User> users = await ReadFromJsonAsync();
+        int maxId = users.Count > 0 ? users.Max(c => c.Id) + 1 : 1;
+        users.Add(entity);
+        await WriteToJsonAsync(users);
+        return entity;
+    }
+
+    public override async Task UpdateAsync(User entity)
+    {
+        List<User> users = await ReadFromJsonAsync();
+        User? userToUpdate = users.SingleOrDefault(c => c.Id == entity.Id);
+        if (userToUpdate == null)
+        {
+            throw new InvalidOperationException($"User with ID '{entity.Id}' not found");
+        }
+
+        users.Remove(userToUpdate);
+        users.Add(entity);
+        await WriteToJsonAsync(users);
+    }
+
+    public override async Task DeleteAsync(int id)
+    {
+        List<User> users = await ReadFromJsonAsync();
+        User? userToUpdate = users.SingleOrDefault(c => c.Id == id);
+        if (userToUpdate == null)
+        {
+            throw new InvalidOperationException($"User with ID '{id}' not found");
+        }
+
+        users.Remove(userToUpdate);
+        await WriteToJsonAsync(users);
+    }
+
+    public override async Task<User> GetSingleAsync(int id)
+    {
+        List<User> users = await ReadFromJsonAsync();
+        User? userToUpdate = users.SingleOrDefault(c => c.Id == id);
+        if (userToUpdate == null)
+        {
+            throw new InvalidOperationException($"User with ID '{id}' not found");
+        }
+
+        return userToUpdate;
+    }
+
+    public override IQueryable<User> GetMany()
+    {
+        List<User> users = ReadFromJsonAsync().Result;
+        return users.Count < 0 ? throw new InvalidOperationException("No users found ") : users.AsQueryable();
+    }
+}
