a5a07fb65c0c606bdf453d365adc95bf72f21971
Alex Tofan <7130188+savaryna@users.noreply.github.com>
2024-09-20 10:33:42 +0200

    add file repository and generic class

---
+++ Server/FileRepositories/GenericFileRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+
+namespace FileRepositories;
+
+public abstract class GenericFileRepository<T>
+{
+  protected abstract string FilePath { get; }
+  private readonly JsonSerializerOptions _writeOptions = new()
+  {
+    WriteIndented = true
+  };
+
+  public GenericFileRepository()
+  {
+    // Initialize the json file with an empty collection
+    if (!File.Exists(FilePath)) File.WriteAllText(FilePath, "[]");
+  }
+
+  public abstract Task<T> AddAsync(T entity);
+
+  public abstract Task UpdateAsync(T entity);
+
+  public abstract Task DeleteAsync(int id);
+
+  public abstract Task<T> GetSingleAsync(int id);
+
+  public abstract IQueryable<T> GetMany();
+
+  protected async Task<List<T>> ReadFromJsonAsync()
+  {
+    string json = await File.ReadAllTextAsync(FilePath);
+    return JsonSerializer.Deserialize<List<T>>(json)!;
+  }
+
+  protected Task WriteToJsonAsync(List<T> value)
+  {
+    string json = JsonSerializer.Serialize(value, _writeOptions);
+    return File.WriteAllTextAsync(FilePath, json);
+  }
+}
